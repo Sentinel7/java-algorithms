3cc1bcbdfc03ae456da83ec013d29eda9d34a823
Jason T <amjbvic@gmail.com>
2018-04-09 20:38:24 -0700

    Add files via upload

---
+++ Algorithms/src/CourseMIT/Food.java
@@ -0,0 +1,31 @@
+package CourseMIT;
+
+public class Food {
+
+	private String name;
+	private int value;
+	private int weight;
+	
+	public Food(String n, int v, int w) {
+		this.name = n;
+		this.value = v;
+		this.weight = w;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public int getWeight() {
+		return this.weight;
+	}
+	
+	public double density() {
+		return 1.0 * value / weight;
+	}
+	
+	public String toString() {
+		return "(" + name + ", " + value + ", " + weight + ")";
+	}
+	
+}
